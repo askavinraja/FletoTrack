c5dda62da8546e4749528a8d74b515678e3289a4
Kavinraja A.S <kavinraja123@gmail.com>
2022-12-07 12:21:15 +0530

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,26 +3,33 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "b09b9bd3e34d01ea30f7bd992be74470",
+  "assets/AssetManifest.json": "a0497566dbf43758d56e8e57d5a35a6b",
+"assets/assets/cbe.json": "99a25594d5d3927f91de84d8651d56c5",
+"assets/assets/london_to_british.json": "0240ad92f13986cd929005a68615daaa",
+"assets/assets/london_to_windsor_castle.json": "bb442cff61570aa4ba855030be6b8c53",
 "assets/assets/maps/as.png": "c7f032c9bc50bddffcfe081443205e45",
 "assets/assets/maps/driverCarMarker.png": "b639e09b7dbede86df7622c908cedee5",
 "assets/assets/maps/map_style.txt": "6360aa79ecc5b32c812dbc4237fc8fe0",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
-"assets/NOTICES": "f8b1dec0c93ed24f08264c531d0ac5d6",
+"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
+"assets/NOTICES": "9c7e7fab80f22ee031f1b1ce61dde7a5",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/packages/fluttertoast/assets/toastify.css": "a85675050054f179444bc5ad70ffc635",
 "assets/packages/fluttertoast/assets/toastify.js": "e7006a0a033d834ef9414d48db3be6fc",
+"canvaskit/canvaskit.js": "c2b4e5f3d7a3d82aed024e7249a78487",
+"canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba",
+"canvaskit/profiling/canvaskit.js": "ae2949af4efc61d28a4a80fffa1db900",
+"canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
 "favicon.ico": "5112fa3ad385c27f13a5a0d14018a6b2",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "97b786c025683a17997d95acf69a4c9c",
 "icons/Icon-512.png": "6e59d1c41b74e2590fa37caf9d1b8bcd",
-"index.html": "44a7e4fa96b0e7e7c69fa84eab78bbf3",
-"/": "44a7e4fa96b0e7e7c69fa84eab78bbf3",
-"main.dart.js": "4b1bcb80729b6cd857e32a5d876bbdda",
+"index.html": "6b6ce5a1c8919f16c6e23c73e1944cbe",
+"/": "6b6ce5a1c8919f16c6e23c73e1944cbe",
+"main.dart.js": "b6043cb55cf3ab6682e64cb6dea988bc",
 "manifest.json": "e3955dccb61866ae34ca4edf1e7c856c",
 "style.css": "408b332bfedc9da6c15d323aa4d61051",
-"version.json": "34081e4dbbe53ccef60e341785de0011"
+"version.json": "9d10dd8f348bab5728dc6857b5458fd1"
 };
 
 // The application shell files that are downloaded before a service worker can
@@ -40,7 +47,7 @@ self.addEventListener("install", (event) => {
   return event.waitUntil(
     caches.open(TEMP).then((cache) => {
       return cache.addAll(
-        CORE.map((value) => new Request(value + '?revision=' + RESOURCES[value], {'cache': 'reload'})));
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
     })
   );
 });
